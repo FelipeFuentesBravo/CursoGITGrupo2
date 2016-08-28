8a8a8b416f263846f2d7faf47819335ae9b997fe
Javier <javier.pinto.g@usach.cl>
2016-08-27 22:59:02 -0300

    Funsion de Division (Javier)
    Funsion que tiene como parametros formales  dos numeros, y que retorna
    el resultado de la division entre estos.

---
+++ funsionDiv.py
@@ -0,0 +1,10 @@
+
+#Funcion que recibe  dos numeros y los divide
+#Entrada: Dos numeros
+#Salida: Un numero
+def dividirNumeros( dividendo, divisor):
+    divisor+=divisor+0.0
+    dividendo+=dividendo+0.0
+    return dividendo/divisor
+
+
