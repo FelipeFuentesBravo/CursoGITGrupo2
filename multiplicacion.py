69ebdded2bb25e81e623ccc5fb90078fc63591ce
FabianEscobar <fabian.escobar@usach.cl>
2016-08-28 22:14:12 -0300

    Función mult (multiplicación)
    Se agrega a función mult, la cual recibe dos números y devuelve el
    producto entre estos.

---
+++ multiplicacion.py
@@ -0,0 +1,9 @@
+#FabianEscobar
+#Curso Git Grupo 2
+
+#Funcion mult (multiplicacion):
+#Entrada: Dos numeros, el multiplicando y el multiplicador
+#Salida: Un numero, resultado de la multiplicacion (producto) 
+def mult(x,y):
+    mult = x*y
+    return mult
