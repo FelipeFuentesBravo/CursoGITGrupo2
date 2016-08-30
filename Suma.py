46ee8907d628e867ceb0e5c54c5d2a3190736409
Mamgui MP <Mamgui.pa@gmail.com>
2016-08-29 22:30:34 -0300

    Se agrega la función Suma

---
+++ Suma.py
@@ -0,0 +1,15 @@
+# -*- coding: cp1252 -*-
+# Función suma de una calculadora
+# Autor: Marcelo G. A. Morales Padilla
+# BLOQUE DE DEFINICIÓN
+
+# DEFINICIÓN DE CONSTANTES
+
+# IMPORTACIÓN DE FUNCIONES
+
+# DEFINICIÓN DE FUNCIONES
+
+
+def suma (uno, dos):
+    return uno + dos
+
